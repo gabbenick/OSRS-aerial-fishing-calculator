a46685e1125a57380d2527755e77167a3665423d
Gabriel Marques <gabbe24081999@gmail.com>
2023-02-22 18:40:41 -0300

    "Experience complete"

--- src/Experience.java
+++ src/Experience.java
@@ -1,12 +1,12 @@
 public class Experience {
 
-    public void Calculator(int[] fishCount) {
+    public void Calculator(int[] fishData) {
 
-        double totalFishingExp = (((double) fishCount[0] * 11.5) + ((double) fishCount[1] * 40.0)
-                + ((double) fishCount[2] * 65.0) + ((double) fishCount[3] * 100.0));
+        double totalFishingExp = (((double) fishData[0] * 11.5) + ((double) fishData[1] * 40.0)
+                + ((double) fishData[2] * 65.0) + ((double) fishData[3] * 100.0));
 
-        double totalHunterExp = (((double) fishCount[0] * 16.5) + ((double) fishCount[1] * 45.0)
-                + ((double) fishCount[2] * 90.0) + ((double) fishCount[3] * 130.0));
+        double totalHunterExp = (((double) fishData[0] * 16.5) + ((double) fishData[1] * 45.0)
+                + ((double) fishData[2] * 90.0) + ((double) fishData[3] * 130.0));
 
         System.out.println("The amount of fishing EXP: " + totalFishingExp);
         System.out.println("The amount of hunter EXP: " + totalHunterExp);
